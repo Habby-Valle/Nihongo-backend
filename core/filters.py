9b98532d467ab597fbd8a682d69ee6906eea56ae
Renildo Rabil <renildorabi@ufrn.edu.br>
2024-02-24 10:13:01 -0300

    refactor: buscabdo por gramatica

--- core/filters.py
+++ core/filters.py
@@ -4,18 +4,29 @@ from core.models import Grammar, Text, Word
 from django.db.models import Q
 
 class GrammarFilter(filters.FilterSet):
+    grammar = filters.CharFilter(field_name='grammar', lookup_expr='icontains')
+    structure = filters.CharFilter(field_name='structure', lookup_expr='icontains')
+    level = filters.NumberFilter(field_name='level')
+    search = filters.CharFilter(method='custom_search')
+
     class Meta:
         model = Grammar
-        fields = {
-            "grammar": ["exact", "icontains"],
-            "structure": ["exact", "icontains"],
-            "level": ["exact"],
-        }
+        fields = ['grammar', 'structure', 'level']
+
+    def custom_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(grammar__icontains=value) | 
+            Q(structure__icontains=value) | 
+            Q(level__icontains=value)
+        )
 
 class WordFilter(filters.FilterSet):
     word = filters.CharFilter(field_name='word', lookup_expr='icontains')
     meaning = filters.CharFilter(field_name='meaning', lookup_expr='icontains')
     reading = filters.CharFilter(field_name='reading', lookup_expr='icontains')
+    level = filters.NumberFilter(field_name='level')
+    type = filters.CharFilter(field_name='type', lookup_expr='icontains')
+    category = filters.CharFilter(field_name='category__name', lookup_expr='icontains')
     search = filters.CharFilter(method='custom_search')
 
     class Meta:
@@ -26,7 +37,10 @@ class WordFilter(filters.FilterSet):
         return queryset.filter(
             Q(word__icontains=value) | 
             Q(meaning__icontains=value) | 
-            Q(reading__icontains=value)
+            Q(reading__icontains=value) |
+            Q(level__icontains=value) |
+            Q(type__icontains=value) |
+            Q(category__name__icontains=value)
         )
 
 class TextFilter(filters.FilterSet):
